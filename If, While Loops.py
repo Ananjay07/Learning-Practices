e6f2bddc582918a62fb516450d867b16ce44e1f6
Ananjay07 <ananjay1357@gmail.com>
2024-06-06 19:58:25 +0700

    Loops

---
+++ If, While Loops.py
@@ -0,0 +1,12 @@
+#for loop (iterate over sequences)
+num = [1, 2, 3, 4, 5,]
+for i in num:
+    if i == 4:
+        break
+    print(i)
+
+#while loop (iterate till a condition is satisfied)
+num = 0
+while num < 4:
+    print(num)
+    num += 1 
