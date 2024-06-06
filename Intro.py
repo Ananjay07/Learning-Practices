3a062cc9c8cc5281b59c6dc99985037bb60e4654
Ananjay07 <ananjay1357@gmail.com>
2024-06-06 19:52:14 +0700

    Intro

---
+++ Intro.py
@@ -0,0 +1,10 @@
+#sep&end
+print("Hello","World", sep=" + ", end="!!\n")
+
+#typecasting
+print(int(4.098))       #float to int
+print(float(8))         #int to float
+print(str(300))         #int/float to string
+print(list("Hi"))       #str to list(separate each variable)
+print(tuple([1, 2, 3, 4, 5]))       #list to tuple
+print(set([1, 2, 3, 4, 5]))         #list to set
