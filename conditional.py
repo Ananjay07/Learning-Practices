f545bb1c00812306fd2e4f70cdce7c4ba9ce8065
Ananjay07 <ananjay1357@gmail.com>
2024-06-06 19:53:26 +0700

    Condition

---
+++ conditional.py
@@ -0,0 +1,8 @@
+#conditional statement
+num = int(input("Enter no:"))
+if num>10:
+    print(f"{num} is greater than 10")
+elif num<10:
+    print(f"{num} is less than 10")
+else:
+    print(f"{num} is equal to 10")
