0169970ee2bdc37fe517480f86b8bdbba19071fc
Ananjay07 <ananjay1357@gmail.com>
2024-06-11 20:21:43 +0700

    Frames

--- 11-6-24/Frame.py
+++ 11-6-24/Frame.py
@@ -8,37 +8,36 @@ def create_widget(parent, widget_type, **options):
 window = create_widget(None, tk.Tk)
 window.title("GUI Example")
 
-# Create a Frame widget with all options
-frame = create_widget(window, tk.Frame, bg='lightblue', bd=3, cursor='hand2', height=100, 
+
+frame = create_widget(window, tk.Frame, bg='lightblue', bd=3, cursor='hand2', height=100,
                       highlightcolor='red', highlightthickness=2, highlightbackground='black', 
                       relief=tk.RAISED, width=200)
 frame.pack(padx=20, pady=20)
+#frame is created for the label
 
-# Create Label widget with all options
-label = create_widget(frame, tk.Label, text='GeeksForGeeks', font='50', bg='lightblue', bd=3, cursor='hand2',
+label = create_widget(frame, tk.Label, text='NAMES', font='50', bg='lightblue', bd=3, cursor='hand2',
                       highlightcolor='red', highlightthickness=2, highlightbackground='black', 
                       relief=tk.RAISED)
 label.pack()
+#a label is created inside the frame
 
-# Create a frame for buttons
 button_frame = create_widget(window, tk.Frame, bg='lightblue', bd=3, cursor='hand2', height=50, 
                               highlightcolor='red', highlightthickness=2, highlightbackground='black', 
                               relief=tk.RAISED, width=200)
 button_frame.pack(pady=10)
+#a frame is created for the buttons
 
-# Function to create buttons with all options
 def create_button(parent, text, fg):
     return create_widget(parent, tk.Button, text=text, fg=fg, bg='lightblue', bd=3, cursor='hand2',
                          highlightcolor='red', highlightthickness=2, highlightbackground='black', 
                          relief=tk.RAISED)
 
-# Create buttons
-buttons_info = [("Geeks1", "red"), ("Geeks2", "brown"), ("Geeks3", "blue"), 
-                ("Geeks4", "green"), ("Geeks5", "green"), ("Geeks6", "green")]
+buttons_info = [("LEWIS", "cyan"), ("MAX", "blue"), ("CHARLES", "red"), 
+                ("BOTTAS", "green"), ("SAINZ", "red")]
 
 for text, fg in buttons_info:
     button = create_button(button_frame, text=text, fg=fg)
     button.pack(side=tk.LEFT)
-
+# the buttons are created inside the frame
 # Run the Tkinter event loop
 window.mainloop()
